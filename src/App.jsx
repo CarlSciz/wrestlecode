6b07ea68ee71ce5eeeacd15ddd1cf2f977d30255
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2024-05-02 16:09:35 -0400

    app.jsx: Added background to header and brought in webscrape data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import './index.css';
+import wrestlerData from './wrestlerData.json';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(wrestlerData);
+  }, []);
+
   return (
     <div className='min-h-screen flex flex-col'>
-      <header className='text-white h-24 flex items-center justify-center fixed top-0 left-0 w-full z-50'>
-        <h1 className='text-9xl font-wrestlemania text-yellow-400 animate-jump-in'>Wrestle Code</h1>
+      <header className='bg-black text-white h-24 flex items-center justify-start fixed top-0 left-0 w-full z-50'>
+        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-5'>Wrestle Code</h1>
       </header>
-      <main className='flex-1 flex items-center justify-center'>
+      <main className='flex-1 flex items-center justify-center mt-24'> 
+        <div className="text-white">
+          <h2 className="text-3xl font-bold mb-4">Wrestler Data</h2>
+          <ul>
+            {data.map((wrestler, index) => (
+              <li key={index}>
+                <strong>{wrestler.gimmick}</strong> - {wrestler.birthplace}, {wrestler.promotion}
+              </li>
+            ))}
+          </ul>
+        </div>
       </main>
     </div>
   );
-};
+}
 
 export default App;
