a114e4c1070a4d1a44b2e0980a534ed90bd193f9
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2024-05-06 15:49:02 -0400

    Link: added working react links to header of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 import wrestlerData from './wrestlerData.json';
 import Wrestlers from './Wrestlers';
@@ -8,10 +9,10 @@ function App() {
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='bg-black text-white h-24 flex items-center justify-start fixed top-0 left-0 w-full z-50'>
-        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-3'>Wrestle Code</h1>
-        <h2 className='text-1xl ml-10'> Wrestlers</h2>
-        <h2 className='text-1xl ml-10'>Promotions</h2>
-        <h2 className='text-1xl ml-10'>About</h2>
+        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-3'> <Link to="/">Wrestle Code</Link></h1>
+        <h2 className='text-1xl ml-10'> <Link to= "/wrestlers">Wrestlers</Link></h2>
+        <h2 className='text-1xl ml-10'><Link to = "/promotion">Promotions</Link></h2>
+        <h2 className='text-1xl ml-10'><Link to = "/about">About</Link></h2>
       </header>
       <div className="mt-24 p-4">
         <h3>Main Page</h3>
--- src/Promotion.jsx
+++ src/Promotion.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Promotions() {
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='bg-black text-white h-24 flex items-center justify-start fixed top-0 left-0 w-full z-50'>
-        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-3'>Wrestle Code</h1>
-        <h2 className='text-1xl ml-10'>Wrestlers</h2>
-        <h2 className='text-1xl ml-10'>Promotions</h2>
-        <h2 className='text-1xl ml-10'>About</h2>
+        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-3'> <Link to="/">Wrestle Code
+        </Link></h1>
+        <h2 className='text-1xl ml-10'> <Link to= "/wrestlers">Wrestlers</Link></h2>
+        <h2 className='text-1xl ml-10'><Link to = "/promotion">Promotions</Link></h2>
+        <h2 className='text-1xl ml-10'><Link to = "/about">About</Link></h2>
       </header>
       <div className="text-white mt-24 p-4">
         <h2>Promotions</h2>
