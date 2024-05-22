4813bdc9ad668fde8dd1d9a6b1adca9f17b19cc5
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2024-05-22 16:40:20 -0400

    wrestlerData: added promotion scraping function

--- src/wrestlePage.cjs
+++ src/wrestlePage.cjs
@@ -20,6 +20,32 @@ async function start() {
     });
 
     await fs.writeFile('wrestlerData.json', JSON.stringify(wrestlerData, null, 2)); // Convert wrestlerData array to JSON and write it to wrestlerData.json
+
+    // Close the Page so we can call another one
+    await page.close();
+
+    // Still using the operating browser we create a new page
+    const page2 = await browser.newPage();
+    await page2.goto("https://www.cagematch.net/?id=8&view=promotions");
+
+
+    //Similar set up as above to extract our promotion data from the website
+    const promotionData = await page2.evaluate(() => {
+        const promotionRows = Array.from(document.querySelectorAll('tr.TRow1, tr.TRow2'));
+        const promotionInfo = promotionRows.map (row => {
+            const columns = row.querySelectorAll('td.TCol');
+            const promotionName = columns[2].querySelector('a').textContent.trim();
+            const promotionHQ = columns[3].textContent.trim();
+            const years = columns[4].textContent.trim();
+            return { promotionName, promotionHQ, years };
+        });
+        return promotionInfo;
+    })
+
+    // Converting all the data from the promotionData to JSON and spitting it out into another file
+    await fs.writeFile('promotionData.json', JSON.stringify(promotionData, null, 2));
+
+    // Finally close the browser after we are finished scraping the data we need
     await browser.close();
 }
 
