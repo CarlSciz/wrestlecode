d2e0ba8bdcbc7c3c8267615094c2897069d0e7f3
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2024-05-22 17:25:19 -0400

    Promotion: added data from json file

--- src/Promotion.jsx
+++ src/Promotion.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import promotionData from './promotionData.json';
 import { Link } from 'react-router-dom';
 
 function Promotions() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(promotionData);
+  }, []);
+
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='bg-black text-white h-24 flex items-center justify-start fixed top-0 left-0 w-full z-50'>
@@ -10,9 +17,19 @@ function Promotions() {
         <h2 className='text-1xl ml-10 font-tna'><Link to = "/promotion">Promotions</Link></h2>
         <h2 className='text-1xl ml-10 font-tna'><Link to = "/about">About</Link></h2>
       </header>
-      <div className="text-white mt-24 p-4">
-        <h2>Promotions</h2>
-        <p>This is the page for Promotion content.</p>
+      <div className="text-white mt-24">
+        <h2 className="text-3xl font-bold mb-4">Wrestling Promotions</h2>
+        <div className="grid grid-cols-1 gap-4">
+          {data.map((item, index) => (
+            <div key={index}>
+              <div className="flex justify-between"> 
+                <strong><a href={item.href} target="_blank">{item.promotionName}</a></strong>
+                <strong className="ml-4">{item.promotionHQ}</strong>
+                <strong className="ml-4">{item.years}</strong> 
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
