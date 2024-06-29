8e5c6d5797c60cde0886025bbd783dbf278f4d0c
CarlSciz <46180364+CarlSciz@users.noreply.github.com>
2024-06-29 12:51:58 -0400

    cursor: added custom cursor and default for links

--- src/Promotion.jsx
+++ src/Promotion.jsx
@@ -22,11 +22,11 @@ function Promotions() {
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='bg-black text-white h-24 flex items-center justify-start fixed top-0 left-0 w-full z-50'>
-        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-3'> <Link to="/">Wrestle Code</Link></h1>
-        <h2 className='text-1xl ml-10 font-tna'> <Link to= "/wrestlers">Wrestlers</Link></h2>
-        <h2 className='text-1xl ml-10 font-tna'><Link to = "/promotion">Promotions</Link></h2>
-        <h2 className='text-1xl ml-10 font-tna'><Link to = "/about">About</Link></h2>
-        <h3 className='text-1xl ml-60 font-tna' onClick={openModal}>Login</h3>
+        <h1 className='text-8xl font-wrestlemania text-yellow-400 animate-jump-in ml-10 mb-3 custom-cursor'> <Link to="/">Wrestle Code</Link></h1>
+        <h2 className='text-1xl ml-10 font-tna custom-cursor'> <Link to= "/wrestlers">Wrestlers</Link></h2>
+        <h2 className='text-1xl ml-10 font-tna custom-cursor'><Link to = "/promotion">Promotions</Link></h2>
+        <h2 className='text-1xl ml-10 font-tna custom-cursor'><Link to = "/about">About</Link></h2>
+        <h3 className='text-1xl ml-60 font-tna custom-cursor' onClick={openModal}>Login</h3>
       </header>
       <div className="text-white mt-24">
         <h2 className="text-3xl font-bold mb-4">Wrestling Promotions</h2>
